2d30b4102949233c8bef981960ad90f30c048f6f
mikeasta <thenaitrise@gmail.com>
2022-09-03 12:01:33 +0300

    Leaderboards memory handling enhanced

--- src/UI/menu_script.js
+++ src/UI/menu_script.js
@@ -1,65 +1,74 @@
-const startGameButton    = document.getElementById("start_btn")
+// Button imports
+const startGameButton = document.getElementById("start_btn")
 const leaderboardsButton = document.getElementById("leaderboards_btn")
 const leaderboardsCloseButton = document.getElementById("leaderboards_close_btn")
 
+// Section imports
 const mainMenu = document.getElementById("main_menu_wrapper")
 const leaderboardsTableWrapper = document.getElementById("leaderboards_wrapper")
 const leaderboardsTable = document.getElementById("leaderboards_table_body")
 
+// Database relative path
 const leaderboardsDatabasePath = "./../../database/dumb_leaderboards.json";
 
-// Game start
+// Game start click
 startGameButton.addEventListener("click", () => {
     leaderboardsTable.style.display = "none";
     mainMenu.style.display = "none";
-});
 
-// Leaderboard opening
-leaderboardsButton.addEventListener("click", async () => {
-
-    // Data loading
-    let database = await(await fetch(leaderboardsDatabasePath,
-        {
-            headers : { 
-                'Content-Type': 'application/json',
-                'Accept': 'application/json'
-            }
-        }
-    )).json();
-
-    let scoresList = database.leaderboards
-
-    // Score array sorting
-    scoresList.sort((a, b) => {
-        return (a.score > b.score) ? -1 : (a.score < b.score) ? 1 : 0
-    })
-
-    // Creating DOM-elements for each player 
-    // Appending them to .html file
-    scoresList.forEach(element => {
-        let newRow = document.createElement("tr")
-        let nicknameCell = document.createElement("td")
-        let scoreCell = document.createElement("td")
-
-        nicknameCell.innerHTML = element["name"]
-        scoreCell.innerHTML = element["score"]
-
-        newRow.append(nicknameCell)
-        newRow.append(scoreCell)
-        leaderboardsTable.append(newRow);
-    });
-
-    // Enable leaderboards table for interaction
-    leaderboardsTableWrapper.style.display = "flex";
-});
-
-leaderboardsCloseButton.addEventListener("click", () => {
     // Memory Cleaning:
     // If nothing refers to DOM-object, JS' GC will delete required elements permanently
     while (leaderboardsTable.firstChild) {
         leaderboardsTable.removeChild(leaderboardsTable.firstChild);
     }
+});
 
+// Leaderboard opening
+leaderboardsButton.addEventListener("click", async () => {
+
+    console.log(leaderboardsTable.childNodes.length)
+
+    // Check if there no any children
+    if (!leaderboardsTable.childNodes.length) {
+        // Data loading
+        let database = await(await fetch(leaderboardsDatabasePath,
+            {
+                headers : { 
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json'
+                }
+            }
+        )).json();
+
+        let scoresList = database.leaderboards
+
+        // Score array sorting
+        scoresList.sort((a, b) => {
+            return (a.score > b.score) ? -1 : (a.score < b.score) ? 1 : 0
+        })
+
+        // Creating DOM-elements for each player 
+        // Appending them to .html file
+        scoresList.forEach(element => {
+            let newRow = document.createElement("tr")
+            let nicknameCell = document.createElement("td")
+            let scoreCell = document.createElement("td")
+
+            nicknameCell.innerHTML = element["name"]
+                scoreCell.innerHTML = element["score"]
+
+            newRow.append(nicknameCell)
+            newRow.append(scoreCell)
+            leaderboardsTable.append(newRow);
+        });
+    }
+    
+    // Enable leaderboards table for interaction
+    leaderboardsTableWrapper.style.display = "flex";
+});
+
+// Leaderboard close
+leaderboardsCloseButton.addEventListener("click", () => {
     // Unable leaderboards table for interaction
     leaderboardsTableWrapper.style.display = "none";
 });
