884a5337ce192255d6126620c1ecb6a34744d986
mikeasta <thenaitrise@gmail.com>
2022-11-22 19:43:37 +0300

    CW: maps are drawed

---
+++ public/assets/map/tileset_2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="space" tilewidth="32" tileheight="32" tilecount="35" columns="7">
+ <image source="../tilesets/space.png" width="240" height="176"/>
+</tileset>
---
+++ public/assets/map/tileset_3.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="Space Sprite Sheet" tilewidth="32" tileheight="32" tilecount="36" columns="6">
+ <image source="../tilesets/Space Sprite Sheet.png" width="192" height="192"/>
+</tileset>
